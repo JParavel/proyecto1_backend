25e07ff40e510f77062ac37c818f7195c1513b5b
paravel <jgamezc@uninorte.edu.co>
2023-05-16 10:27:15 -0500

    Plantilla REST

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import userRouter from './user/user.router';
-import restaurantRouter from './restaurant/restaurant.router';
-import productRouter from './product/product.router';
-import orderRouter from './order/order.router';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use('/users', userRouter);
-app.use('/restaurants', restaurantRouter);
-app.use('/products', productRouter);
-app.use('/orders', orderRouter);
-
-mongoose
-	.connect('mongodb+srv://cluster0.y2hf4m5.mongodb.net/', {
-		dbName: 'proyecto',
-		user: 'test',
-		pass: 'test',
-	})
-	.then(() => console.log('database connected'))
-	.catch((error) => console.log(error));
 
 try {
 	app.listen(port);
