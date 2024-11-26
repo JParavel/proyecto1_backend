0a558414cebf602724122f03f8da296950e263e9
SrParavel <jgamezc@uninorte.edu.co>
2024-11-26 18:20:09 -0500

    Agregue AWITA

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => res.status(200).json({ status: '🐋' }));
 
 try {
 	app.listen(port);
-	console.log('server running on http://localhost:' + port);
+	console.log('server running on http://localhost: AWITA' + port);
 } catch (error) {
 	console.log(error);
 }
